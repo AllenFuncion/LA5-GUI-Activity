565ead868cbb1d09e4f01441cd38eacf43d857e9
Allen <vafuncion@gmail.com>
2022-12-20 19:51:32 +0800

    Rename SimpleCalcGUI components

--- LabActivity5-master/src/SimpleCalc/SimpleCalcGUI.java
+++ LabActivity5-master/src/SimpleCalc/SimpleCalcGUI.java
@@ -3,8 +3,18 @@ package SimpleCalc;
 import javax.swing.*;
 
 public class SimpleCalcGUI {
+    private JPanel mainPanel;
+    private JTextField tf1Number;
+    private JComboBox cbSymbol;
+    private JButton btnResult;
+    private JPanel mainPanel1;
     private JPanel panel1;
-    private JTextField textField1;
-    private JComboBox comboBox1;
-    private JButton computeResultButton;
+    private JLabel jl1Number;
+    private JLabel jl2Number;
+    private JPanel panel2;
+    private JTextField tf2Number;
+    private JPanel panel3;
+    private JLabel jlResult;
+    private JTextField tfResult;
+    private JPanel resultPanel;
 }
