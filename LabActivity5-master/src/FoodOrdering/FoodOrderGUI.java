533de967a44ce684e8fb460d773af0b65b84e9ab
Allen <vafuncion@gmail.com>
2022-12-20 18:58:38 +0800

    Rename FoodOrderGUI components

---
+++ LabActivity5-master/src/FoodOrdering/FoodOrderGUI.java
@@ -0,0 +1,29 @@
+package FoodOrdering;
+
+import javax.swing.*;
+
+public class FoodOrderGUI {
+    private JLabel jlFoods;
+    private JCheckBox cbBurger;
+    private JCheckBox cbFries;
+    private JCheckBox cbSoftDrinks;
+    private JCheckBox cbTea;
+    private JCheckBox cbSundae;
+    private JLabel jl100;
+    private JLabel jl80;
+    private JLabel jl65;
+    private JLabel jl55;
+    private JLabel jl50;
+    private JLabel jl40;
+    private JLabel jlDiscount;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+    private JRadioButton rbNone;
+    private JCheckBox cbPizza;
+    private JPanel pricePanel;
+    private JPanel discountPanel;
+    private JPanel btnPanel;
+    private JPanel mainPanel;
+    private JButton btnOrder;
+}
