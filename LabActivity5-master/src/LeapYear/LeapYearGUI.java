4230618e440627f5a8f5669229d85b5d2a659bc3
Allen <vafuncion@gmail.com>
2022-12-20 19:25:23 +0800

    Add LeapYearGUI main function

--- LabActivity5-master/src/LeapYear/LeapYearGUI.java
+++ LabActivity5-master/src/LeapYear/LeapYearGUI.java
@@ -2,11 +2,16 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame {
 
     private JPanel mainPanel1;
     private JPanel mainPanel2;
     private JLabel jlYear;
     private JTextField trYear;
     private JButton btnCheckYear;
+
+    public static void main(String[] args) {
+
+    }
 }
+
