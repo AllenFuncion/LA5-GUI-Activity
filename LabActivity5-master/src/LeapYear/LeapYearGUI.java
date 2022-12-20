2d8d1f5bc454147f70368f366cefc97cb1b5671d
Allen <vafuncion@gmail.com>
2022-12-20 19:30:05 +0800

    Complete LeapYearGUI

--- LabActivity5-master/src/LeapYear/LeapYearGUI.java
+++ LabActivity5-master/src/LeapYear/LeapYearGUI.java
@@ -1,17 +1,38 @@
 package LeapYear;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LeapYearGUI extends JFrame {
 
     private JPanel mainPanel1;
     private JPanel mainPanel2;
     private JLabel jlYear;
-    private JTextField trYear;
+    private JTextField tfYear;
     private JButton btnCheckYear;
-
+    public LeapYearGUI() {
+        setContentPane(mainPanel1);
+        setTitle("Leap Year Checker");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String year = tfYear.getText();
+                int y = Integer.parseInt(year);
+                JFrame f = new LeapYearGUI();
+                if (((y % 4 == 0) && (y % 100!= 0)) || (y % 400 == 0))
+                    JOptionPane.showMessageDialog(f, "Leap year");
+                else {
+                    JOptionPane.showMessageDialog(f, "Not a leap year");
+                }
+            }
+        });
+    }
     public static void main(String[] args) {
-
+        LeapYearGUI leapYearChecker = new LeapYearGUI();
     }
 }
 
